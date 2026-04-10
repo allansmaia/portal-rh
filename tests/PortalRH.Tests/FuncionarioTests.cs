fbfb4c5cec4a28ae41197e43b32f7bf88805fb90
allansmaia <84999755+allansmaia@users.noreply.github.com>
2026-04-10 06:30:39 -0300

    feature: Estrutura Inicial do projeto com CI (#1)
    * feature: Estrutura Inicial do projeto com CI
    
    * chore: Adiciona template de PR com Tags Convencionais

---
+++ tests/PortalRH.Tests/FuncionarioTests.cs
@@ -0,0 +1,35 @@
+using PortalRH;
+
+namespace PortalRH.Tests;
+
+public class FuncionarioTests
+{
+    [Fact]
+    public void CalcularBonus_DeveRetornarValorCorreto()
+    {
+        var funcionario = new Funcionario("Ana Silva", "Desenvolvedora", 5000m);
+
+        var bonus = funcionario.CalcularBonus(10);
+
+        Assert.Equal(500m, bonus);
+    }
+
+    [Fact]
+    public void CalcularBonus_PercentualNegativo_DeveLancarExcecao()
+    {
+        var funcionario = new Funcionario("Carlos Lima", "Analista", 4000m);
+
+        var ex = Record.Exception(() => funcionario.CalcularBonus(-5));
+        Assert.IsType<ArgumentException>(ex);
+    }
+
+    [Fact]
+    public void Funcionario_DeveTerPropriedadesCorretas()
+    {
+        var funcionario = new Funcionario("Maria Costa", "Gerente", 8000m);
+
+        Assert.Equal("Maria Costa", funcionario.Nome);
+        Assert.Equal("Gerente", funcionario.Cargo);
+        Assert.Equal(8000m, funcionario.Salario);
+    }
+}
